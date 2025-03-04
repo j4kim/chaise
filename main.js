fabebf97bce516f79f730fc91ddf3d4521d9dd51
Joaquim Perez <jivkim@gmail.com>
2025-03-04 22:23:05 +0100

    change camera fov, direction, bg color, light color, enable antialiasing

--- main.js
+++ main.js
@@ -2,23 +2,26 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xff0000);
 const camera = new THREE.PerspectiveCamera(
-    75,
+    45,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
+const light = new THREE.DirectionalLight(0xff5555, 3);
 light.position.set(2, 2, 2);
 scene.add(light);
 
-camera.position.z = 5;
+camera.position.z = 2;
+camera.position.y = 0.3;
+camera.rotation.x = -0.15;
 
 const loader = new GLTFLoader();
 
